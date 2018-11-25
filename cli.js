71a9a80bdc42134ef089e91f4995a648b8fc9c67
Dessalines <happydooby@gmail.com>
2018-11-25 11:31:34 -0700

    - Adding a command line CLI - Force adding /announce to tracker url - Using maxPeers instead of averaging - Adding name, length, infohash, created, completed, to output

---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var torrentHealth = require('./index.js');
+// var torrentHealth = require('torrent-tracker-health');
+
+var argv = require('minimist')(process.argv.slice(2));
+
+if (argv._.length !== 0 || !argv.torrent) {
+  console.error([
+    'Usage: torrent-tracker-health [options]',
+    '  --torrent: the torrent file or magnet link',
+    '  --timeout: millisecond timeout',
+    '  --addTrackers={tracker1/announce, tracker2/announce} or --addTrackers=tracker1/announce',
+  ].join('\n'));
+  process.exit(1);
+} else {
+  var timeout = (!argv.timeout) ? 400 : argv.timeout;
+  var opts = {};
+  opts.timeout = argv.timeout;
+  opts.force = argv.addTrackers;
+
+  if (argv.torrent) {
+    torrentHealth(argv.torrent, opts).then(function (res) {
+      console.log(res);
+
+    }).catch(function (err) {
+      console.log('error:', err);
+    }).then(() => {
+      process.exit(1);
+    });
+  }
+}
+
